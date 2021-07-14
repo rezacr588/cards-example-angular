892d446d28aff9d7113d26d498ad386c4cb059b6
Reza Zeraat <rezacr588@gmail.com>
2021-07-14 23:30:09 +0430

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards';
+  posts = [
+    {
+      title: "Neat Tree",
+      username: "@nature",
+      content: "Saw this awesome tree during my hike today.",
+      imageURL: "assets/tree.jpeg"
+    },
+    {
+      title: "Snowy Mountain",
+      username: "@mountainLover",
+      content: "here is a picture of snowy mountain",
+      imageURL: "assets/mountain.jpeg"
+    },
+    {
+      title: "Mountain Biking",
+      username: "@biking22",
+      content: "I did some biking in this place",
+      imageURL: "assets/biking.jpeg"
+    },
+  ]
 }
